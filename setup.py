36c8a85673f53d93e2d0a260d663256a243acb45
RL <ruizhe.lin@scilifelab.se>
2025-07-30 10:07:12 +0200

    delete requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='miao',
     version='1.0',
-    packages=find_packages(),
+    packages=[],
     entry_points={
         'console_scripts': [
             'miao = miao.__main__:main'
