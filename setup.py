766cb6fb76afd17d552345dbd87acc22be71f1d9
Spexophis <billinpro@gmail.com>
2025-08-05 16:47:42 +0200

    remove requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='miao',
     version='1.0',
-    packages=find_packages(),
+    packages=[],
     entry_points={
         'console_scripts': [
             'miao = miao.__main__:main'
